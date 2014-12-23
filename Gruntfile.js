702077ceffc42690919a2f7eec1eef0af2679df4
Remco Tolsma <info@remcotolsma.nl>
2014-12-23 09:03:33 +0100

    Added text domain check.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,13 +24,47 @@ module.exports = function( grunt ) {
 					exclude: [ 'bower_components/.*', 'node_modules/.*' ],
 				}
 			}
-		}
+		},
+
+		// Check textdomain errors
+		checktextdomain: {
+			options:{
+				text_domain: 'orbis_pronamic',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php',
+					'!deploy/**',
+					'!node_modules/**',
+					'!tests/**',
+					'!wp-content/**'
+				],
+				expand: true
+			}
+		},
 	} );
 
 	grunt.loadNpmTasks( 'grunt-phplint' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
 
 	// Default task(s).
 	grunt.registerTask( 'default', [ 'phplint' ] );
-	grunt.registerTask( 'pot', [ 'makepot' ] );
+	grunt.registerTask( 'pot', [ 'checktextdomain', 'makepot' ] );
 };
